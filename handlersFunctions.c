30247ea514796fab44673d829112244c837a21b5
BKaokine <b.koukin@gmail.com>
2023-04-23 22:59:48 +0000

    Task 0 : Header and HandlerFunctions Files

--- handlersFunctions.c
+++ handlersFunctions.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
-void handle_char(va_list *arg_list, int *chars_printed, char *buffer)
+void handle_char(va_list *arg_list, int *chars_printed)
 {
 char c = va_arg(*arg_list, int);
 write(1, &c, 1);
 (*chars_printed)++;
 }
 
-void handle_string(va_list *arg_list, int *chars_printed, char *buffer)
+void handle_string(va_list *arg_list, int *chars_printed)
 {
 char *s = va_arg(*arg_list, char *);
 while (*s != '\0')
@@ -18,8 +18,8 @@ s++;
 }
 }
 
-void handle_percent(va_list *arg_list, int *chars_printed, char *buffer)
-{
+void handle_percent(va_list *arg_list, int *chars_printed) {
+(void)arg_list;  /* suppress unused parameter warning */
 write(1, "%", 1);
 (*chars_printed)++;
 }
--- main.h
+++ main.h
@@ -8,14 +8,14 @@
 
 /* Functions Prototypes*/
 int _printf(const char *format, ...);
-void handle_char(va_list *arg_list, int *chars_printed, char *buffer);
-void handle_string(va_list *arg_list, int *chars_printed, char *buffer);
-void handle_percent(va_list *arg_list, int *chars_printed, char *buffer);
+void handle_char(va_list *arg_list, int *chars_printed);
+void handle_string(va_list *arg_list, int *chars_printed);
+void handle_percent(va_list *arg_list, int *chars_printed);
 
 /* Structs Variables*/
 typedef struct conversion_handler {
     char specifier;
-    void (*handler)(va_list *, int *, char *);
+    void (*handler)(va_list *, int *);
 } conversion_handler;
 
 
