b8585c3130ce34c61efa09c31ce3dae8670aba97
BKaokine <b.koukin@gmail.com>
2023-04-25 11:46:18 +0000

    new implimentation (but over 40 lines)

--- _printf.c
+++ _printf.c
@@ -7,84 +7,83 @@
  */
 int _printf(const char *format, ...)
 {
-    char *null = "(null)";
-    
-    if (format == NULL)
-        return (-1);
+char *null = "(null)";
+int count = 0;
+va_list args;
 
-    va_list args;
-    va_start(args, format);
-    int count = 0;
+if (format == NULL)
+return (-1);
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
+va_start(args, format);
+while (*format)
+{
+if (*format == '%')
+{
+format++;
 
-            if (*format == '\0')
-                return (-1);
+if (*format == '\0')
+return (-1);
 
-            switch (*format)
-            {
-                case 's':
-                {
-                    char *str = va_arg(args, char*);
-                    int i , j;
-                    if (str == NULL)
-                    {
-            			for (j = 0; *(null + j) != '\0'; j++)
-            			{
-            				putchar(*(null + j));
-            				count++;
-            			}
-                    }
-                   else
-                   {
-               			for (i = 0; str[i] != '\0'; i++)
-            			{
-            				putchar(str[i]);
-                            count++;
-            			}
+switch (*format)
+{
+case 's':
+{
+char *str = va_arg(args, char*);
+int i, j;
+if (str == NULL)
+{
+for (j = 0; *(null + j) != '\0'; j++)
+{
+putchar(*(null + j));
+count++;
+}
+}
+else
+{
+for (i = 0; str[i] != '\0'; i++)
+{
+putchar(str[i]);
+count++;
+}
 
-                   }
-                    break;
-                }
-                case 'c':
-                {
-                    char c = (char)va_arg(args, int);
-                    putchar(c);
-                    count++;
-                    break;
-                }
-                case '%':
-                    putchar('%');
-                    count++;
-                    break;
-                case ' ':
-                    putchar(' ');
-                    count++;
-                    while (*(format+1) == ' ')
-                    {
-                        format++;
-                    }
-                    break;
-                default:
-                    putchar('%');
-                    putchar(*format);
-                    count += 2;
-                    break;
-            }
-        }
-        else
-        {
-            putchar(*format);
-            count++;
-        }
+}
+break;
+}
+case 'c':
+{
+char c = (char)va_arg(args, int);
+putchar(c);
+count++;
+break;
+}
+case '%':
+putchar('%');
+count++;
+break;
+case ' ':
+putchar(' ');
+count++;
+while (*(format + 1) == ' ')
+{
+format++;
+}
+break;
+default:
+putchar('%');
+putchar(*format);
+count += 2;
+break;
+}
+}
+else
+{
+putchar(*format);
+count++;
+}
 
-        format++;
-    }
+format++;
+}
 
-    va_end(args);
-    return count;
+va_end(args);
+return (count);
 }
